9d192005af7e82d73dd5ea4270c1960055923939
Mihail Simbirskiy <simbirsky@bk.ru>
2023-07-24 20:25:44 +0300

    Added yellow card event type

--- src/Pages/Match/Tabs/MatchSummary/EventType.php
+++ src/Pages/Match/Tabs/MatchSummary/EventType.php
@@ -6,6 +6,7 @@ namespace Simbirskiy\FlashscoreParser\Pages\Match\Tabs\MatchSummary;
 
 enum EventType: string
 {
-    case PENALTY = '10';
     case OTHER = '0';
+    case YELLOW_CARD = '1';
+    case PENALTY = '10';
 }
